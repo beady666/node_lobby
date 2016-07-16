1dd6eedbc44832cb6095fc4c06399a6c1ad3d5c5
beady666 <abbos@pobox.com>
2016-07-15 19:16:15 -0500

    require login to access pages

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res) {
+router.get('/', checkAuth, function(req, res) {
     res.render('index');
 });
 
+function checkAuth(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  else {
+    //req.flash('error_msg', 'You must log in');
+    res.redirect('/users/login');
+  }
+}
+
 module.exports = router;
+
+
